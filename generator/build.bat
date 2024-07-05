6b3fb79b097784b1892ae6b3a24a44283d0ee6db
atrepalin <atrepalin19@gmail.com>
2024-07-05 13:55:05 +0300

    Update build script

--- generator/build.bat
+++ generator/build.bat
@@ -1,1 +1,1 @@
-pyinstaller --noconfirm --onefile --console generator/main.py --distpath resources/extra -n generator.exe --icon resources/icon.ico
+pyinstaller --noconfirm --onefile --console generator/main.py --distpath resources/extra -n generator.exe --icon resources/icon.ico --hidden-import fastapi
